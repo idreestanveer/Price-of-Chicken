be02f5be73e785cb19cc164148386f7a4dfe80c5
hasnain <hasnainawan428@gmail.com>
2022-10-28 19:13:54 +0500

    My first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.write('<h1>BUY CHICKEN EARLY</h1>')
+var prise = 650
+var weight = prompt('(The rate of chicken is 650kg) How many gram do you want to buy')
+var amount = prise / 1000 * weight
+document.write('Their are the price fo chicken you slected :', amount)
+
